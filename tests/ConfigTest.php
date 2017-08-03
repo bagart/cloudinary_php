88e8a3e4b41e8e55a57ac1b7d0bee8995879797c
RTLcoil <oleg@rtl.co.il>
2017-08-03 23:42:01 +0300

    SDK-803: Add notification_url to the `update` Admin API method (PHP) - Fixed tests to support PHP 5.3

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -8,8 +8,9 @@ class ConfigTest extends TestCase
     public function test_config_nested_values()
     {
         \Cloudinary::config_from_url('cloudinary://key:secret@test123?foo[bar]=value');
-        $this->assertArrayHasKey('foo', \Cloudinary::config());
-        $this->assertArrayHasKey('bar', \Cloudinary::config()['foo']);
-        $this->assertEquals('value', \Cloudinary::config()['foo']['bar']);
+        $config = \Cloudinary::config();
+        $this->assertArrayHasKey('foo', $config);
+        $this->assertArrayHasKey('bar', $config['foo']);
+        $this->assertEquals('value', $config['foo']['bar']);
     }
 }
